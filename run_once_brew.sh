5400c7b42074043dbcad14b13bcc5fcafd204c34
Bryan Winstead <bryan@winstead.io>
2022-03-01 16:27:17 -0500

    fix: Make brew install non-interactive

--- run_once_brew.sh
+++ run_once_brew.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-NON_INTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew bundle install --file ~/.config/brew/Brewfile
